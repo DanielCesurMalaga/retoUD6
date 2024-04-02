3e66c1f4b12d5bd1cb974cf9d254f20f931d9989
DanielCesurMalaga <daniel.fernandezcampos@cesurformacion.com>
2024-04-02 13:54:19 +0200

    Segundo

---
+++ src/Jugador.java
@@ -0,0 +1,45 @@
+import java.time.LocalTime;
+
+public class Jugador {
+    // atributos
+    private String nombre;
+    private String arma;
+    private String habitacion;
+    private LocalTime horaPartida;
+    // constructor
+    public Jugador(){
+        this.nombre=null;
+        this.arma=null;
+        this.habitacion=null;
+        this.horaPartida=null;
+    }
+    // getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getArma() {
+        return arma;
+    }
+    public void setArma(String arma) {
+        this.arma = arma;
+    }
+    public String getHabitacion() {
+        return habitacion;
+    }
+    public void setHabitacion(String habitacion) {
+        this.habitacion = habitacion;
+    }
+    public LocalTime getHoraPartida() {
+        return horaPartida;
+    }
+    public void setHoraPartida() {
+        this.horaPartida = LocalTime.now();
+    }
+    public static void main(String[] args) {
+        Jugador jugador = new Jugador();
+
+    }   
+}
